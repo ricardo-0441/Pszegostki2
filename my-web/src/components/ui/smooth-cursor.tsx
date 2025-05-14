570d784f864215917f4d0bed1dbe1da1f9f90fa7
igna08 <ignaciolibutti@gmail.com>
2025-05-14 18:36:36 -0300

    1.0

--- my-web/src/components/ui/smooth-cursor.tsx
+++ my-web/src/components/ui/smooth-cursor.tsx
@@ -89,6 +89,7 @@ export function SmoothCursor({
     restDelta: 0.001,
   },
 }: SmoothCursorProps) {
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [isMoving, setIsMoving] = useState(false);
   const lastMousePos = useRef<Position>({ x: 0, y: 0 });
   const velocity = useRef<Position>({ x: 0, y: 0 });
